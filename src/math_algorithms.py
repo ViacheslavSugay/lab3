452cf860898a9b2cc6a156888503ab1834add6f4
ViacheslavSugay <vdsugaj@mai.education>
2025-12-08 15:21:11 +0300

    добавил ошибку в факториале

--- src/math_algorithms.py
+++ src/math_algorithms.py
@@ -1,4 +1,8 @@
 def factorial(n: int) -> int:
+
+    if n < 0:
+        raise ValueError('Должно быть неотрицательное число')
+
     start = 1
     for i in range(1, n + 1):
         start *= i
@@ -6,6 +10,9 @@ def factorial(n: int) -> int:
 
 
 def factorial_recursive(n: int) -> int:
+    if n < 0:
+        raise ValueError('Должно быть неотрицательное число')
+
     if n == 0 or n == 1:
         return 1
     return n * factorial_recursive(n - 1)
